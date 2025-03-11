22684eeb552dda46fa04dc48f66e8678c61a4742
sagarkarki903 <sagarkarki903@gmail.com>
2025-03-11 01:53:37 -0500

    fix3

--- client/src/components/newDashboard/CalendarPage.jsx
+++ client/src/components/newDashboard/CalendarPage.jsx
@@ -4,7 +4,7 @@ import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { NewScheduler } from '../scheduling/NewScheduler';
 import axios from 'axios';
-import { fetchUser } from "../utils/userService";
+import { fetchUser } from '../../utils/userService';
 
 const localizer = momentLocalizer(moment);
 

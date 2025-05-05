39072a245d8518577bd63e9014b2594938c44e8e
Sagar <sagarkarki903@gmail.com>
2025-05-04 22:02:00 -0500

    added global axios with credential to be true in main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import axios from 'axios'; // 👈 Import axios
+import './index.css';
+import App from './App.jsx';
+
+// ✅ Set this globally to ensure all requests include cookies
+axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
